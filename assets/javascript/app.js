e9c7ea5d8500a491aa56e3bee2effbea1c522ec0
AR123456 <coloradoz99@gmail.com>
2018-12-09 07:48:17 -0700

    JS Local Storage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,43 @@
-function debounce(func, wait = 20, immediate = true) {
-  var timeout;
-  return function() {
-    var context = this,
-      args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelector(".plates");
+const items = JSON.parse(localStorage.getItem("items")) || [];
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector("[name=item ]").value;
+  const item = {
+    text,
+    done: false
   };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+  this.reset();
+}
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+                                                                                                             <li> 
+                                                                                                             <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      } />
+                                                                                                              <label for ="item${i}"> ${
+        plate.text
+      }</label>
+                                                                                                              </li>
+                                                                                                               `;
+    })
+    .join("");
+}
+function toggleDone(e) {
+  if (!e.target.matches("input")) return; // skip this ubless its and input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done; //flipping true false
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
 }
 
-const sliderImages = document.querySelectorAll(".slide-in");
-function checkSlide() {
-  sliderImages.forEach(sliderImage => {
-    //math
-    //half way through the image
-    const slideInAt =
-      window.scrollY + window.innerHeight - sliderImage.height / 2;
-    //bottom of the image
-    const imageBottom = sliderImage.offsetTop + sliderImage.height;
-    // is image half shown
-    const isHalfShown = slideInAt > sliderImage.offsetTop;
-    const isNotScrolledPast = window.scrollY < imageBottom;
-    if (isHalfShown && isNotScrolledPast) {
-      sliderImage.classList.add("active");
-    } else {
-      sliderImage.classList.remove("active");
-    }
-  });
-}
-window.addEventListener("scroll", debounce(checkSlide));
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+populateList(items, itemsList);
