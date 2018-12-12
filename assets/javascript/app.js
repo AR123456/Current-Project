c429e184432dbcff212cb139df5732ee035cf411
AR123456 <coloradoz99@gmail.com>
2018-12-12 03:25:08 -0700

    JS web cam

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,105 @@
-const bands = [
-  "The Plot in You",
-  "The Devil Wears Prada",
-  "Pierce the Veil",
-  "Norma Jean",
-  "The Bled",
-  "Say Anything",
-  "The Midway State",
-  "We Came as Romans",
-  "Counterparts",
-  "Oh, Sleeper",
-  "A Skylit Drive",
-  "Anywhere But Here",
-  "An Old Dog"
-];
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
 
-function strip(bandName) {
-  return bandName.replace(/^(a |the |an )/i, "").trim();
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.src = window.URL.createObjectURL(localMediaStream);
+      video.play();
+    })
+    .catch(err => {
+      console.error(`OH NO!!!`, err);
+    });
 }
-// const sortedBands = bands.sort(function(a, b) {
-//   if (strip(a) > strip(b)) {
-//     return 1;
-//   } else {
-//     return -1;
-//   }
-// });
 
-//  same using turnary operator
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
 
-// const sortedBands = bands.sort(function(a, b) {
-//   return strip(a) > strip(b) ? 1 : -1;
-// });
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with them
+    // pixels = redEffect(pixels);
 
-// with arrow and implicit  return
-const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+    pixels = rgbSplit(pixels);
+    // ctx.globalAlpha = 0.8;
 
-document.querySelector("#bands").innerHTML = sortedBands
-  .map(band => `<li>${band}</li>`)
-  .join("");
+    // pixels = greenScreen(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
 
-console.log(sortedBands);
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "thepic");
+  link.innerHTML = `<img src="${data}" alt="pic" />`;
+  strip.insertBefore(link, strip.firsChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll(".rgb input").forEach(input => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (
+      red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax
+    ) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
+
+getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
