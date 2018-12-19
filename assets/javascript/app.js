2137b7624c342c9d223169a33f20ca3584b72ca6
AR123456 <coloradoz99@gmail.com>
2018-12-19 02:23:10 -0700

    JS-Puppy-Nav_Follow-You-Mouse

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,44 @@
-const nav = document.querySelector("#main");
-const topOfNav = nav.offsetTop;
+const triggers = document.querySelectorAll(".cool > li");
+const background = document.querySelector(".dropdownBackground");
+const nav = document.querySelector(".top");
 
-function fixNav() {
-  if (window.scrollY >= topOfNav) {
-    document.body.paddingTop = nav.offsetHeight + "px";
-    document.body.classList.add("fixed-nav");
-  } else {
-    document.body.paddingTop = 0;
-    document.body.classList.remove("fixed-nav");
-  }
+function handleEnter() {
+  this.classList.add("trigger-enter");
+  setTimeout(
+    () =>
+      this.classList.contains("trigger-enter") &&
+      this.classList.add("trigger-enter-active"),
+    150
+  );
+  background.classList.add("open");
+
+  const dropdown = this.querySelector(".dropdown");
+  const dropdownCoords = dropdown.getBoundingClientRect();
+  const navCoords = nav.getBoundingClientRect();
+
+  const coords = {
+    height: dropdownCoords.height,
+    width: dropdownCoords.width,
+    top: dropdownCoords.top - navCoords.top,
+    left: dropdownCoords.left - navCoords.left
+  };
+
+  background.style.setProperty("width", `${coords.width}px`);
+  background.style.setProperty("height", `${coords.height}px`);
+  background.style.setProperty(
+    "transform",
+    `translate(${coords.left}px, ${coords.top}px)`
+  );
 }
 
-window.addEventListener("scroll", fixNav);
+function handleLeave() {
+  this.classList.remove("trigger-enter", "trigger-enter-active");
+  background.classList.remove("open");
+}
+
+triggers.forEach(trigger =>
+  trigger.addEventListener("mouseenter", handleEnter)
+);
+triggers.forEach(trigger =>
+  trigger.addEventListener("mouseleave", handleLeave)
+);
