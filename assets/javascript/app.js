20057138ea665fe24ac5a5cbae47fde1cf08911b
AR123456 <coloradoz99@gmail.com>
2018-12-28 06:27:56 -0700

    Playing around with ES6 arrow functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,16 @@
-// select the second hand div element
-const secondHand = document.querySelector(".second-hand");
-// select the min hand div element
-const minHand = document.querySelector(".min-hand");
-// select the hour hand div element
-const hourHand = document.querySelector(".hour-hand");
+const box = document.querySelector(".box");
 
-function setDate() {
-  //using JS Date function
-  const now = new Date();
-  //*************** second hand ***********
-  //using JS getSeconds function
-  const seconds = now.getSeconds();
-  //convert to base 10 for the degrees, add 90 deg to offset the original 90 deg used to orient the clock face up and down vs side to side
-  const secondsDegrees = (seconds / 60) * 360 + 90;
-  // apply styles to the second hand selected and made into const above,ES6 template literals
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+box.addEventListener("click", function() {
+  let first = "opening";
+  let second = "open";
 
-  //*******************min hand************
-  //using JS get min function
-  const min = now.getMinutes();
-  // min to base 10 and take care of offset
-  const minsDegrees = (min / 60) * 360 + 90;
-  // apply styles ot the min hand const , ES 6 template literals
-  minHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-  //*******************Hour hand************
-  //using JS get min function
-  const hour = now.getHours();
-  // min to base 10 and take care of offset
-  const hoursDegrees = (hour / 12) * 360 + 90;
-  // apply styles ot the min hand const , ES 6 template literals
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-}
-setInterval(setDate, 1000);
-
-//TODO Use Javascript if statements to fix the weird jump when the hands hit 12 take off the transition that is on line 45  of styles so that when it gets to the next one it will not transition itself backwards  and then re apply it after( inside of the set date  )
+  if (this.classList.contains(first)) {
+    [first, second] = [second, first];
+  }
+  this.classList.toggle(first);
+  setTimeout(() => {
+    //use the arrow function here so that it is inherited from the function ( parent) vs from the window
+    console.log(this);
+    this.classList.toggle(second);
+  }, 500);
+});
