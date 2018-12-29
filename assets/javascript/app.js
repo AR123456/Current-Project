b4eb424737d998b0007d8fb121b757d033f6eb06
AR123456 <coloradoz99@gmail.com>
2018-12-29 08:03:38 -0700

    JS ES^ playing with map filter and reduce

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,21 @@
-const box = document.querySelector(".box");
+// Select all the list items on the page and convert to array
+// use this way (wrapping the document.querySelectorAll in Array.from) for max browser support since querySelectorAll returns a node list, not a true array but browser support is comming
+const items = Array.from(document.querySelectorAll("[data-time]"));
+// if newer browser this will work
+// const items = document.querySelectorAll("[data-time]");
 
-box.addEventListener("click", function() {
-  let first = "opening";
-  let second = "open";
+// Filter for only the elements that contain the word 'Flexbox'
+const filtered = items
+  .filter(item => item.textContent.includes("Flexbox"))
+  // map down to a list of time strings
+  .map(item => item.dataset.time) //data-time
+  // map to an array of seconds
+  .map(timecode => {
+    const parts = timecode.split(":").map(part => parseFloat(part));
+    return parts[0] * 60 + parts[1];
+  })
+  // reduce to get total
+  .reduce((runningTotal, seconds) => runningTotal + seconds);
+console.log(filtered);
 
-  if (this.classList.contains(first)) {
-    [first, second] = [second, first];
-  }
-  this.classList.toggle(first);
-  setTimeout(() => {
-    //use the arrow function here so that it is inherited from the function ( parent) vs from the window
-    console.log(this);
-    this.classList.toggle(second);
-  }, 500);
-});
+// 🔥 This can also be done in a single .reduce(), but we're practicing arrow functions here, so chain them!
