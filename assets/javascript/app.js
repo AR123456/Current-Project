4f1a597b6eca9c0060e7227a986e10189ef62101
AR123456 <coloradoz99@gmail.com>
2018-12-31 06:58:07 -0700

    ES6 Tagged Templates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,35 @@
-const panels = document.querySelectorAll(".panel");
-function toggleOpen() {
-  this.classList.toggle("open");
+const dict = {
+  HTML: "Hyper Text Markup Language",
+  CSS: "Cascading Style Sheets",
+  JS: "JavaScript"
+};
+
+function addAbbreviations(strings, ...values) {
+  //need new array of values for if a abbreviation wrap in a abbr tag.
+  const abbreviated = values.map(value => {
+    //check if the dictionary has a key for the value return the dictionary item with the matching key
+    if (dict[value]) {
+      return `<abbr title="${dict[value]}">${value}</abbr> `;
+    }
+    //if it is not in the dictionary just return it
+    return value;
+  });
+  //use reduce to return the string takes function and what yu start with
+  return strings.reduce((sentence, string, i) => {
+    return `
+     ${sentence}${string}${abbreviated[i] || ""}
+    `;
+  }, "");
 }
-function toggleActive(e) {
-  if (e.propertyName.includes("flex")) {
-    this.classList.toggle("open-active");
-  }
-}
-panels.forEach(panel => panel.addEventListener("click", toggleOpen));
-panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+const first = "Anne";
+const last = "Rottler";
+//in this sentence the HTML , CSS and JS are being passed in as strings to be interpelated
+const sentence = addAbbreviations`Hello my name is ${first} ${last} and I love to code ${"HTML"}, ${"CSS"} and ${"JS"}`;
+
+const bio = document.querySelector(".bio");
+const p = document.createElement("p");
+p.innerHTML = sentence;
+bio.appendChild(p);
+
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/abbr
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
