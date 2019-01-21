cf6d2491973a78ef06eb9e71bd7c83cbf9892e36
AR123456 <coloradoz99@gmail.com>
2019-01-21 04:54:19 -0700

    ES6 Generatros

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,63 @@
-const panels = document.querySelectorAll(".panel");
-function toggleOpen() {
-  this.classList.toggle("open");
-}
-function toggleActive(e) {
-  if (e.propertyName.includes("flex")) {
-    this.classList.toggle("open-active");
+// the astrics is the notation to make this a yeild function 
+function* listPeople() {
+  // with yield this function would need to be run 3 times to get all the names 
+  let i = 0;
+  yield i;
+  i++;
+  yield i;
+  i++;
+  yield i;
+  // yield "Anne";
+  // yield "Alicia";
+  // yield "Becky";
+
+};
+
+//  to invoke this function and store it in a variable 
+const people = listPeople();
+//call .next on the generator, this returns an object  (in the console)
+
+const inventors = [{
+    first: 'Albert',
+    last: 'Einstein',
+    year: 1879
+  },
+  {
+    first: 'Isaac',
+    last: 'Newton',
+    year: 1643
+  },
+  {
+    first: 'Galileo',
+    last: 'Galilei',
+    year: 1564
+  },
+  {
+    first: 'Marie',
+    last: 'Curie',
+    year: 1867
+  },
+  {
+    first: 'Johannes',
+    last: 'Kepler',
+    year: 1571
+  },
+  {
+    first: 'Nicolaus',
+    last: 'Copernicus',
+    year: 1473
+  },
+  {
+    first: 'Max',
+    last: 'Planck',
+    year: 1858
+  },
+];
+
+function* loop(arr) {
+  console.log(inventors)
+  for (const item of arr) {
+    yield item;
   }
 }
-panels.forEach(panel => panel.addEventListener("click", toggleOpen));
-panels.forEach(panel => panel.addEventListener("transitionend", toggleActive));
+const inventorGen = loop(inventors);
