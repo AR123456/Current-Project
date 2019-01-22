28ba52cd6f97fae0da97295cc7362698b5c1f077
AR123456 <coloradoz99@gmail.com>
2019-01-22 05:36:27 -0700

    Generators looping

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,18 @@
-// the astrics is the notation to make this a yeild function 
-function* listPeople() {
-  // with yield this function would need to be run 3 times to get all the names 
-  let i = 0;
-  yield i;
-  i++;
-  yield i;
-  i++;
-  yield i;
-  // yield "Anne";
-  // yield "Alicia";
-  // yield "Becky";
+// loop a line of a song 
 
-};
 
-//  to invoke this function and store it in a variable 
-const people = listPeople();
-//call .next on the generator, this returns an object  (in the console)
-
-const inventors = [{
-    first: 'Albert',
-    last: 'Einstein',
-    year: 1879
-  },
-  {
-    first: 'Isaac',
-    last: 'Newton',
-    year: 1643
-  },
-  {
-    first: 'Galileo',
-    last: 'Galilei',
-    year: 1564
-  },
-  {
-    first: 'Marie',
-    last: 'Curie',
-    year: 1867
-  },
-  {
-    first: 'Johannes',
-    last: 'Kepler',
-    year: 1571
-  },
-  {
-    first: 'Nicolaus',
-    last: 'Copernicus',
-    year: 1473
-  },
-  {
-    first: 'Max',
-    last: 'Planck',
-    year: 1858
-  },
-];
-
-function* loop(arr) {
-  console.log(inventors)
-  for (const item of arr) {
-    yield item;
-  }
+function* lyrics() {
+  //notice this is in back tickes so its dynamic 
+  yield `But don't tell my heart`;
+  yield `My achy breaky heart`;
+  yield `I just don't think he'd understand`;
+  yield `And if you tell my heart`;
+  yield `My achy breaky heart`;
+  yield `He might blow up and kill this man`;
 }
-const inventorGen = loop(inventors);
+
+const achy = lyrics();
+//can use this instead of manualy calling .next 
+for (const line of achy) {
+  console.log(line);
+}
