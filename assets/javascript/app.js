f61862448fc3b6f39f3281d6fd3fc15f53c251ae
AR123456 <coloradoz99@gmail.com>
2019-01-22 05:37:44 -0700

    Generators data flow

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,35 @@
-// loop a line of a song 
+// using generators to make a waterfall API request
 
-
-function* lyrics() {
-  //notice this is in back tickes so its dynamic 
-  yield `But don't tell my heart`;
-  yield `My achy breaky heart`;
-  yield `I just don't think he'd understand`;
-  yield `And if you tell my heart`;
-  yield `My achy breaky heart`;
-  yield `He might blow up and kill this man`;
+function ajax(url) {
+  fetch(url).then(data => data.json()).then(data => dataGen.next(data))
 }
 
-const achy = lyrics();
-//can use this instead of manualy calling .next 
-for (const line of achy) {
-  console.log(line);
+
+
+
+// generator function for steps 
+function* steps() {
+  // making 3 ajax requests that relie on the one before 
+  console.log('fetching beers');
+  const beers = yield ajax('http://api.react.beer/v2/search?q=hops&type=beer');
+  console.log(beers);
+
+  // if the data is needed for the second request it could be done this way //////////
+  // console.log('fetching wes');
+  // const wes = yield ajax('https://api.github.com/users/' + beers[0]);
+  // console.log(wes);
+
+
+
+  console.log('fetching wes');
+  const wes = yield ajax('https://api.github.com/users/wesbos');
+  console.log(wes);
+
+  console.log('fetching fat joe');
+  const fatJoe = yield ajax('https://api.discogs.com/artists/51988');
+  console.log(fatJoe);
+
+
 }
+const dataGen = steps();
+dataGen.next(); // kicks if off
