aab5113788abe1270952f571b3bb50739881af47
AR123456 <coloradoz99@gmail.com>
2019-01-23 05:56:58 -0700

    ES6 Proxies

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,43 @@
-// using generators to make a waterfall API request
+// 
 
-function ajax(url) {
-  fetch(url).then(data => data.json()).then(data => dataGen.next(data))
-}
+const person = {
+  name: "Anne",
+  age: 29
+};
 
+//when getting name want to return something else 
+// proxy takes 2 things a object and a handler 
+//in handle specify all the oberations that need re writing  - this is called a trap
+//trap is between you and the object , 
+// const personProxy = new Proxy(person, {
+//   //this is the trap
+//   get(target, name) {
+//     console.log("Someone is asking for ", target, name);
+//     return " Nahhhhh"
+//   }
+// });
 
+// can use this to modify or check to make sure something is meeting standars like to upper case 
 
+// const person = {
+//   name: "Anne",
+//   age: 29
+// };
 
-// generator function for steps 
-function* steps() {
-  // making 3 ajax requests that relie on the one before 
-  console.log('fetching beers');
-  const beers = yield ajax('http://api.react.beer/v2/search?q=hops&type=beer');
-  console.log(beers);
+//when getting name want to return something else 
+// proxy takes 2 things a object and a handler 
+//in handle specify all the oberations that need re writing  - this is called a trap
+//trap is between you and the object , 
+const personProxy = new Proxy(person, {
+  //this is the trap
+  get(target, name) {
+    return target[name].toUpperCase();
+  },
+  //could use set to remove white space
+  set(target, name, value) {
+    if (typeof value === "string") {
+      target[name] = value.trim().toUpperCase() + "Gone";
 
-  // if the data is needed for the second request it could be done this way //////////
-  // console.log('fetching wes');
-  // const wes = yield ajax('https://api.github.com/users/' + beers[0]);
-  // console.log(wes);
-
-
-
-  console.log('fetching wes');
-  const wes = yield ajax('https://api.github.com/users/wesbos');
-  console.log(wes);
-
-  console.log('fetching fat joe');
-  const fatJoe = yield ajax('https://api.discogs.com/artists/51988');
-  console.log(fatJoe);
-
-
-}
-const dataGen = steps();
-dataGen.next(); // kicks if off
+    }
+  }
+});
