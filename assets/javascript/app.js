82de29d5aa56982d9499e680bb6e0ecef8806bf7
AR123456 <coloradoz99@gmail.com>
2019-01-25 04:41:23 -0700

    ES6 Sets

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,29 @@
-//phone number handler 
-const phoneHandler = {
-  set(target, name, value) {
-    target[name] = value.match(/[0-9]/g).join("");
-  },
-  get(target, name) {
-    return target[name].replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
-  }
+// the set API
+const people = new Set();
+people.add("Judy");
+people.add("Peggy");
+people.add("Rosie");
+console.log(people.size);
+// can also do people.delete("Judy") no need to indicate the position like in an array
+// people.clear would remove all of them
+//entrys , keys and values are also avalible.
+// the setIterator can also be used on it
+const it = people.values();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+// can feed it into a for of loop( have to use for of to loop it not just a for )
+for (const person of people) {
+  console.log(person);
 }
+//methonds of keys and entries  - helpful in map
 
-const phoneNumbers = new Proxy({}, phoneHandler);
+const students = new Set(["Steve", "Tom", "Paul"]);
+
+/// can pass an array into  a set
+const dogs = ["Pixie", "Brandy"];
+const dogSet = new Set(dogs);
+console.log(dogSet);
+// can use .has to see if someone is in it .
+// cannot repeat an entry
