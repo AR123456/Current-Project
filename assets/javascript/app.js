ff9ba986299c521e16891e831607d3936be51460
AR123456 <coloradoz99@gmail.com>
2019-01-25 04:43:10 -0700

    ES6 weak set

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,12 @@
-// the set API
-const people = new Set();
-people.add("Judy");
-people.add("Peggy");
-people.add("Rosie");
-console.log(people.size);
-// can also do people.delete("Judy") no need to indicate the position like in an array
-// people.clear would remove all of them
-//entrys , keys and values are also avalible.
-// the setIterator can also be used on it
-const it = people.values();
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-console.log(it.next());
-// can feed it into a for of loop( have to use for of to loop it not just a for )
-for (const person of people) {
-  console.log(person);
-}
-//methonds of keys and entries  - helpful in map
+let dog1 = { name: "Pixie", age: 3 };
+let dog2 = { name: "Sunny", age: 1 };
 
-const students = new Set(["Steve", "Tom", "Paul"]);
+const weakSauce = new WeakSet([dog1, dog2]);
 
-/// can pass an array into  a set
-const dogs = ["Pixie", "Brandy"];
-const dogSet = new Set(dogs);
-console.log(dogSet);
-// can use .has to see if someone is in it .
-// cannot repeat an entry
+console.log(weakSauce);
+// a weak set has no .clear method ,they clean themselfs up - garbage collection in memory.  When the ref to the dog is removed it will automatically be removed from the set
+
+// may need to wait a couple of seconds
+dog1 = null;
+
+console.log(weakSauce);
