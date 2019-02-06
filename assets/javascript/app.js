263fb029c40449ba6cdd085c9e1f8b38f52add36
AR123456 <coloradoz99@gmail.com>
2019-02-06 04:04:03 -0700

    HTML Canvas and D3.JS Template

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,32 @@
-// .some and .every  - are not part of ES6 but helpful
-// will check the items in the array to see if some meet what you are looking for
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var cw = canvas.width;
+var ch = canvas.height;
 
-const ages = [32, 15, 19, 12];
-//is there an adult in the group
-const adultPresent = ages.some(age => age >= 18);
-console.log(adultPresent);
-// is eveyone over 18 ?
-const isEveryone = ages.every(age => age >= 21);
-console.log(isEveryone);
+// define your barchart as a data-array of javascript objects
+var bars = [
+  { x: 50, y: ch, width: 20, height: 125, color: "green" },
+  { x: 80, y: ch, width: 20, height: 80, color: "red" },
+  { x: 110, y: ch, width: 20, height: 225, color: "blue" }
+];
+
+// draw the barchart based on the data array
+drawBars();
+
+function drawBars() {
+  ctx.textAlign = "center";
+  ctx.textBaseline = "bottom";
+  ctx.font = "14px verdana";
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, cw, ch);
+  for (var i = 0; i < bars.length; i++) {
+    var bar = bars[i];
+    ctx.fillStyle = bar.color;
+    ctx.fillRect(bar.x, bar.y - 20, bar.width, -bar.height);
+    ctx.fill();
+    ctx.strokeStyle = "lightgray";
+    ctx.strokeRect(bar.x, bar.y - 20, bar.width, -bar.height);
+    ctx.fillStyle = "white";
+    ctx.fillText(bar.height, bar.x + bar.width / 2, ch - 3);
+  }
+}
