ec5e81c2e6ae3b5f53e97d33b12dafa7452e49a9
AR123456 <coloradoz99@gmail.com>
2019-02-07 04:48:17 -0700

    fibonacci in the console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,18 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var cw = canvas.width;
-var ch = canvas.height;
+function fibonacciGenerator(n) {
+  //TODO  every number after the first 2 is the some of the 2 proceeding numbers
 
-// define your barchart as a data-array of javascript objects
-var bars = [
-  { x: 50, y: ch, width: 20, height: 125, color: "green" },
-  { x: 80, y: ch, width: 20, height: 80, color: "red" },
-  { x: 110, y: ch, width: 20, height: 225, color: "blue" }
-];
+  let fib = [];
+  let num1 = 0;
+  let num2 = 1;
+  let num3 = 1;
 
-// draw the barchart based on the data array
-drawBars();
-
-function drawBars() {
-  ctx.textAlign = "center";
-  ctx.textBaseline = "bottom";
-  ctx.font = "14px verdana";
-  ctx.fillStyle = "black";
-  ctx.fillRect(0, 0, cw, ch);
-  for (var i = 0; i < bars.length; i++) {
-    var bar = bars[i];
-    ctx.fillStyle = bar.color;
-    ctx.fillRect(bar.x, bar.y - 20, bar.width, -bar.height);
-    ctx.fill();
-    ctx.strokeStyle = "lightgray";
-    ctx.strokeRect(bar.x, bar.y - 20, bar.width, -bar.height);
-    ctx.fillStyle = "white";
-    ctx.fillText(bar.height, bar.x + bar.width / 2, ch - 3);
+  for (let i = 0; i < n; i++) {
+    num3 = num1 + num2;
+    num1 = num2;
+    num2 = num3;
+    console.log(fib.push(num3));
   }
+
+  //Return an array of fibonacci numbers.
 }
+fibonacciGenerator(5);
