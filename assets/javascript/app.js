53310dc4bb0f65c573f720915a57a258307337a9
AR123456 <coloradoz99@gmail.com>
2019-02-08 04:38:32 -0700

    Fizz Buzz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,51 @@
-function fibonacciGenerator(n) {
-  //TODO  every number after the first 2 is the some of the 2 proceeding numbers
+// use while for state
+// while(someting is true){
+//   do something
+// }
+// use for to iterate
+// for (let i = 0; i < array.length; i++) {
+//   do something
 
-  let fib = [];
-  let num1 = 0;
-  let num2 = 1;
-  let num3 = 1;
+// }
 
-  for (let i = 0; i < n; i++) {
-    num3 = num1 + num2;
-    num1 = num2;
-    num2 = num3;
-    console.log(fib.push(num3));
+//Fizz Buzz
+// Write a program that prints the numbers from 1 to 100  but for multiples of three print "Fizz" instead of the number and for the multiples of five print "buzz". For numbers which are multiples of both three and five print "FizzBuzz"
+let fizzBuzzy = function() {
+  let num = [];
+  for (let i = 1; num.length < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      num.push(" fizz buzz");
+    } else if (i % 3 === 0) {
+      num.push("fizz");
+    } else if (i % 5 === 0) {
+      num.push("buzz");
+    } else {
+      num.push(i);
+    }
   }
 
-  //Return an array of fibonacci numbers.
+  console.log(num);
+};
+fizzBuzzy();
+
+// another fiz buzz  using  while
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      output.push("Fizz");
+    } else if (count % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(count);
+    }
+    count++;
+  }
+  console.log(output);
 }
-fibonacciGenerator(5);
+fizzBuzz();
