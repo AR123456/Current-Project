af5efa93d42a0c6f271d419a1e03698a1bcf06da
AR123456 <coloradoz99@gmail.com>
2019-02-09 06:27:24 -0700

    Arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,10 @@
-//Love Calculator
-prompt("What is your name ? ");
-prompt("What is the name of your hearts desire ? ");
-function LoveNum() {
-  // var n = Math.random();
+//arrays
+var guestList = ["Angela", "Jack", "Pam", "James", "Laura", "Jason"];
+// array  .includes  .length
+var name = prompt("What is your name ?");
 
-  // n = Math.floor(n * 100) + 1;
-
-  var n = Math.floor(Math.random() * 100 + 1);
-  if (n > 70) {
-    alert("Your love score is:  " + n + "% the perfect love");
-  }
-  if (n < 70 && n > 30) {
-    alert("Your love score is:  " + n + "% that is an average love");
-  }
-  if (n < 30) {
-    alert("Your love score is:  " + n + "% keep looking !");
-  }
+if (guestList.includes(name)) {
+  alert("Welcome to the party");
+} else {
+  alert("You are not on the list.");
 }
-LoveNum();
