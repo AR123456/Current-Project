c552f433083187c878b7d83f96bdefe656e78d39
AR123456 <coloradoz99@gmail.com>
2019-02-09 06:28:40 -0700

    BMI Calculator

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,20 @@
-//arrays
-var guestList = ["Angela", "Jack", "Pam", "James", "Laura", "Jason"];
-// array  .includes  .length
-var name = prompt("What is your name ?");
-
-if (guestList.includes(name)) {
-  alert("Welcome to the party");
-} else {
-  alert("You are not on the list.");
+function bmiCalc(k, m) {
+  bmi = k / (m ^ 2);
+  console.log(bmi);
+  if (bmi < 18.5) {
+    return "Underweight";
+  }
+  if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Nomral ";
+  }
+  if (bmi > 24.99 && bmi <= 29.9) {
+    return "Overweight";
+  }
+  if (bmi > 29.99 && bmi <= 34.9) {
+    return "Obese";
+  }
+  if (bmi > 34.9) {
+    return "Extremly Obese";
+  }
 }
+bmiCalc(78, 1.5);
