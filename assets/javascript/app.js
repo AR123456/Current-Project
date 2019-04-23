b16aabfe072d17567168ea1a6cb5d5fae04901a2
AR123456 <coloradoz99@gmail.com>
2019-04-23 04:18:52 -0600

    Simple Canvas shadows

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,66 @@
-var btn = document.querySelector("#btn");
+// making sure window loads before the canvas starts
+window.onload = function() {
+  // a simple line
+  var canvas = document.getElementById("hello-world-canvas");
+  var ctx = canvas.getContext("2d");
 
-//listen for clicks
-btn.onclick = function() {
-  var XHR = new XMLHttpRequest();
-  XHR.onreadystatechange = function() {
-    if (XHR.readyState == 4 && XHR.status == 200) {
-      var url = JSON.parse(XHR.responseText).message;
-      img.src = url;
-    }
-  };
+  ctx.beginPath();
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = "green";
+  ctx.lineJoin = "round";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = 15;
+  ctx.shadowOffsetY = 15;
+  ctx.shadowBlur = 5;
+  ctx.moveTo(30, 30);
+  ctx.lineTo(130, 30);
+  // ctx.lineTo(130, 130);
+  // ctx.lineTo(30, 130);
+  // ctx.lineTo(30, 230);
+  ctx.stroke();
 
-  XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
-  XHR.send();
+  ctx.beginPath();
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = "red";
+  ctx.lineJoin = "round";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = -15;
+  ctx.shadowOffsetY = 15;
+  ctx.shadowBlur = 5;
+  ctx.moveTo(60, 60);
+  ctx.lineTo(160, 60);
+  // ctx.lineTo(160, 160);
+  // ctx.lineTo(60, 160);
+  // ctx.lineTo(60, 260);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = "blue";
+  ctx.lineJoin = "round";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = -15;
+  ctx.shadowOffsetY = 15;
+  ctx.shadowBlur = 5;
+  ctx.moveTo(90, 90);
+  ctx.lineTo(190, 90);
+  // ctx.lineTo(190, 190);
+  // ctx.lineTo(90, 190);
+  // ctx.lineTo(90, 290);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.lineWidth = 20;
+  ctx.strokeStyle = "yellow";
+  ctx.lineJoin = "round";
+  ctx.shadowColor = "grey";
+  ctx.shadowOffsetX = -15;
+  ctx.shadowOffsetY = -15;
+  ctx.shadowBlur = 5;
+  ctx.moveTo(110, 110);
+  ctx.lineTo(220, 110);
+  // ctx.lineTo(220, 220);
+  // ctx.lineTo(110, 220);
+  // ctx.lineTo(110, 320);
+  ctx.stroke();
 };
