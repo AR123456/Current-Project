01e58b7fc0fe31f9daa8fe54b9a09396cb61f802
AR123456 <coloradoz99@gmail.com>
2019-04-25 04:54:49 -0600

    JS Callback functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,15 @@
-var timerId = setTimeout(function() {
-  console.log("This function runs in 30 seconds");
-}, 30000);
-setTimeout(function() {
-  console.log("Canceling the first setTimeout", timerId);
-  clearTimeout(timerId);
-}, 2000);
+// //////////// using findIndex//////////////////
+var arr = [1, 9, 4, 7, 6, 5, 8, 10, 2, 3];
 
-var num = 0;
-var intervalId = setInterval(function() {
-  num++;
-  console.log("num:", num);
-  if (num === 3) {
-    clearInterval(intervalId);
-  }
-}, 1000);
+// findIndex(arr, function(num, index, array) {
+//   return num === 6;
+// });
+//resulte returned is  4  since 6 is in the 4th position in the array
+
+// findIndex(arr, function(num, index, array) {
+//   return num % 2 === 0;
+// });
+//  / result of finding position of numbers in the array that are divisible by 2
+//2, or the first even number
+
+console.log(arr.indexOf(7));
