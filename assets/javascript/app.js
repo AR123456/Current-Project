e96fbd3e47f608e754a84e6c3eef25e2f116cbc6
AR123456 <coloradoz99@gmail.com>
2019-04-26 06:15:40 -0600

    JS Promise

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,99 +1,25 @@
-// nested async call backs//////////////////
-// code is hard to read
-// logic is difficult to reason out
-// code is not modular
-
-// var counter = 0;
-// setTimeout(function() {
-//   counter++;
-//   console.log("Counter: ", counter);
-//   setTimeout(function() {
-//     counter++;
-//     console.log("Counter: ", counter);
-//     setTimeout(function() {
-//       counter++;
-//       console.log("Counter: ", counter);
-//     }, 3000);
-//   }, 2000);
-// }, 1000);
-
-////////////Returning a promise with promise chaining
-
-// var promise = new Promise(function(resolve, reject) {
-//   setTimeout(function() {
-//     randomInt = Math.floor(Math.random() * 10);
-//     resolve(randomInt);
-//   }, 500);
+// Promise example
+// var p1 = new Promise(function(resolve, reject) {
+//   var num = Math.random();
+//   if (num < 0.5) {
+//     resolve(num);
+//   } else {
+//     reject(num);
+//   }
+// });
+// p1.then(function(result) {
+//   console.log("Success: ", result);
+// }).catch(function(error) {
+//   console.log("Error: ", error);
 // });
 
-// promise
-//   .then(function(data) {
-//     console.log("Random int passed to resolve: ", data);
-//     return new Promise(function(resolve, reject) {
-//       setTimeout(function() {
-//         resolve(Math.floor(Math.random() * 10));
-//       }, 3000);
-//     });
-//   })
-//   .then(function(data) {
-//     console.log("Second random int passed to resolve: ", data);
-//   });
-
-// ///////// Promise Chaining  Returning Data
-// var promise = new Promise(function(resolve, reject) {
-//   resolve(5);
-// });
-// promise
-//   .then(function(data) {
-//     return data * 2;
-//   })
-//   .then(function(data) {
-//     return data + 20;
-//   })
-//   .then(function(data) {
-//     console.log(data);
-//   });
-
-//// nested call back refactor
-// var counter = 0;
-// setTimeout(function() {
-//   counter++;
-//   console.log("Counter: ", counter);
-//   setTimeout(function() {
-//     counter++;
-//     console.log("Counter: ", counter);
-//     setTimeout(function() {
-//       counter++;
-//       console.log("Counter: ", counter);
-//     }, 3000);
-//   }, 2000);
-// }, 1000);
-
-///////////////////////////////// refactored ///////////////
-var counter = 0;
-// create function declaration
-function incCounter() {
-  counter++;
-  console.log("Counter: ", counter);
-}
-// create a runLater function ///////
-function runLater(callback, timeInMs) {
-  var p = new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      var res = callback();
-      resolve(res);
-    }, timeInMs);
-  });
-  return p;
-}
-// Chain Promises
-runLater(incCounter, 1000)
-  .then(function() {
-    return runLater(incCounter, 2000);
-  })
-  .then(function() {
-    return runLater(incCounter, 3000);
-  })
-  .then(function() {
-    //final.then not necessary
-  });
+//Promise with asyncromis code
+var promise = new Promise(function(resolve, reject) {
+  setTimeout(function() {
+    var randomInt = Math.floor(Math.random() * 10);
+    resolve(randomInt);
+  }, 4000);
+});
+promise.then(function(data) {
+  console.log("Random init passed to resolve: ", data);
+});
