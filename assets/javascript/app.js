6450d57866c54b27875f6d1363bb65277c9ca590
AR123456 <coloradoz99@gmail.com>
2019-04-30 04:36:11 -0600

    JS Random dog image app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,15 @@
 var btn = document.querySelector("#btn");
-var priceDisp = document.querySelector("#price");
+
 //listen for clicks
 btn.onclick = function() {
   var XHR = new XMLHttpRequest();
   XHR.onreadystatechange = function() {
     if (XHR.readyState == 4 && XHR.status == 200) {
-      // var url = JSON.parse(XHR.responseText).message;
-      // var price = (document.getElementById("#price").innerHTML =
-      //   XHR.message.bpi.usd.rate_float);
-      var data = JSON.parse(XHR.responseText);
-      var price = data.bpi.USD.rate_float;
-      priceDisp.innerText = price;
+      var url = JSON.parse(XHR.responseText).message;
+      img.src = url;
     }
   };
 
-  XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+  XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
   XHR.send();
 };
