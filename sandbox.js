6dd798fa233ba906306c954bdae51148d4979999
AR123456 <coloradoz99@gmail.com>
2019-09-08 07:31:27 -0600

    foreach and callbacks

--- sandbox.js
+++ sandbox.js
@@ -1,44 +1,12 @@
-//old school function
-// let people = ["mario", "luigi", "ryu", "shaun", "chun-li"];
-// people.forEach(function(person) {
-//   console.log(person);
+// callbacks & forEach
+let people = ['mario', 'luigi', 'ryu', 'shaun', 'chun-li'];
+
+const logPerson = (person, index) => {
+  console.log(`${index} - hello ${person}`);
+}
+
+// people.forEach(person => {
+//   console.log(`hello ${person}`);
 // });
 
-// passing into
-
-// let people = ["mario", "luigi", "ryu", "shaun", "chun-li"];
-// const logPerson = (person, index) => {
-//   console.log(`${index} - hellow ${person}`);
-// };
-// people.forEach(logPerson);
-
-// callback in action - putting on the page //////////
-// const ul = document.querySelector(".people");
-
-// const people = ["mario", "luigi", "ryu", "shaun", "chun-li"];
-
-// let html = ``;
-
-// people.forEach(function(person) {
-//   // create htmel template
-//   html += `<li style="color: purple">${person}</li>`;
-// });
-
-// console.log(html);
-
-// ul.innerHTML = html;
-
-//as arrow/////
-const ul = document.querySelector(".people");
-
-const people = ["mario", "luigi", "ryu", "shaun", "chun-li"];
-
-let html = ``;
-
-people.forEach(person => {
-  // create html template for each person
-  html += `<li style="color: purple">${person}</li>`;
-});
-
-console.log(html);
-ul.innerHTML = html;
+people.forEach(logPerson);
