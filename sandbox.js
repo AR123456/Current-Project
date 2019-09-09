edbb5095e09175eb644d0fc37ac81f09da44f0d4
AR123456 <coloradoz99@gmail.com>
2019-09-09 05:39:14 -0600

    adding methods

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,31 @@
 // object literals
-// key value pairs
+
 let user = {
   name: "crystal",
   age: 30,
   email: "crystal@thenetninja.co.uk",
   location: "berlin",
-  blogs: ["why mac & cheese rules", "10 things to make with marmite"]
+  blogs: ["why mac & cheese rules", "10 things to make with marmite"],
+  // attach method to this object
+  // methods - doing things the key is what we want to do, (login) and the value is doing it the function
+  // name and what it does
+  login: function() {
+    console.log("the user logged in");
+  },
+  logout: function() {
+    console.log("the user logged out");
+  },
+  logBlogs: function() {
+    // access the blogs here
+    console.log(user.blogs);
+  }
 };
 
-console.log(user);
-console.log(user.age);
+//call the methods defined above
+user.login();
+user.logout();
+user.logBlogs();
 
-user.age = 35;
-console.log(user.age);
-
-// can use [] but dot notation is more commone and likley preferred
-console.log(user["name"]);
-user["name"] = "chun-li";
-console.log(user["name"]);
-
-console.log(typeof user);
-console.log(user);
+const name = "shaun";
+name.toUpperCase();
+// Method  :  a function defined on an object
