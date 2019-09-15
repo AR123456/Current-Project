52aa04f391514260c1a938367937a02d73af190e
AR123456 <coloradoz99@gmail.com>
2019-09-15 05:06:08 -0600

    event delegation and bubbling

--- sandbox.js
+++ sandbox.js
@@ -1,27 +1,31 @@
 const ul = document.querySelector("ul");
-// ul.remove();
-
 const button = document.querySelector("button");
 
-//// one way to add element to DOM
-// button.addEventListener("click", () => {
-//   ul.innerHTML += "<li>a new thing to do </li>";
-// });
-/////another way to add something to the DOM
 button.addEventListener("click", () => {
   const li = document.createElement("li");
   li.textContent = "something new to do";
-  ////adds to the bottom of the parent
-  //ul.appendChild(li);
-  ///// add to the top of the parent
-  ul.prepend(li);
+  ul.appendChild(li);
 });
 
-const items = document.querySelectorAll("li");
+// const items = document.querySelectorAll('li');
 
-items.forEach(item => {
-  item.addEventListener("click", e => {
-    // e.target.style.textDecoration = 'line-through';
+// items.forEach(item => {
+//   item.addEventListener('click', e => {
+//     console.log('event in LI');
+//     e.stopPropagation();
+//     e.target.remove();
+//   });
+// });
+
+/// add the event listener here so that it gets applied to dynamicaly created tags above
+
+ul.addEventListener("click", e => {
+  console.log('event in UL');
+ // /find out what was clicked
+  console.log(e.target, e);
+  if (e.target.tagName === "LI") {
     e.target.remove();
-  });
+  }
+
+ 
 });
