7778eac8b56037affaed230ef18b757dcafb5f4b
AR123456 <coloradoz99@gmail.com>
2019-09-26 06:23:29 -0600

    find method

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,11 @@
-// reduce can return any value you want based on the values in the array that are iterated over like a number, string or new array
-//callback funtion perfomed on each value
+// the find method returns the value of the first item in an array that passes a certain test in a callback function
+// once the value is found it stops and returns it.
+const scores = [10, 5, 0, 40, 60, 10, 20, 70];
 
-const scores1 = [10, 20, 60, 40, 70, 90, 30];
-// call back takes in accumulator(a running total) and current
-const result = scores1.reduce((acc, curr) => {
-  if (curr > 50) {
-    acc++;
-  }
-  return acc;
-  //passing an argument which is the inital value of the accumulator of zero
-}, 0);
-
-console.log(result);
-
-const scores = [
-  { player: "mario", score: 50 },
-  { player: "yoshi", score: 30 },
-  { player: "mario", score: 70 },
-  { player: "crystal", score: 60 }
-];
-
-const marioTotal = scores.reduce((acc, curr) => {
-  if (curr.player === "mario") {
-    acc += curr.score;
-  }
-  return acc;
-}, 0);
-
-console.log(marioTotal);
+const firstHighScore = scores.find(score => {
+  // returns true or false, stops when true is found and returns the value to the variable
+  return score > 50;
+});
+// short hand way to wright
+const firstHighScore = scores.find(score => score > 50);
+console.log(firstHighScore);
