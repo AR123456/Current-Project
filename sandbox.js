9b0f6f530ee10abf8f88f57f2eeb267759ab35c6
AR123456 <coloradoz99@gmail.com>
2019-09-28 08:32:12 -0600

    building a digital clock

--- sandbox.js
+++ sandbox.js
@@ -1,21 +1,19 @@
-//const before = new Date('02/01/2019 7:30:59');
-// passing is string to get the date object of that date
-const before = new Date("February 1 2019 7:30:59");
-const now = new Date();
-// getTime() is a number representing the number of miliseconds since 12am on the 1st of January 1970
-const diff = now.getTime() - before.getTime();
-// const diff = now - before;  gets the number of mili seconds between the 2 dates
+const clock = document.querySelector(".clock");
+// get time every second
+const tick = () => {
+  const now = new Date();
 
-console.log(diff);
-
-const mins = Math.round(diff / 1000 / 60);
-const hours = Math.round(mins / 60);
-const days = Math.round(hours / 24);
-
-console.log(`the before date was ${mins} mins ago`);
-console.log(`the before date was ${hours} hours ago`);
-console.log(`the before date was ${days} days ago`);
-
-// converting timestamps to dates
-const timestamp = 1675938474990;
-console.log(new Date(timestamp));
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+  // injecting HTML template into the DOM
+  const html = `
+    <span>${h}</span> :
+    <span>${m}</span> :
+    <span>${s}</span>
+  `;
+  // every seocnd updating the html
+  clock.innerHTML = html;
+};
+// pass in the tick, function and run it every second
+setInterval(tick, 1000);
