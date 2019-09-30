a91d9fd8189484c2574d3bbb6160d8d96ba52e77
AR123456 <coloradoz99@gmail.com>
2019-09-30 05:39:49 -0600

    promise basics

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,54 @@
-const getTodos = (resource, callback) => {
+const getTodos = resource => {
+  // JS Promise
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  const request = new XMLHttpRequest();
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("could not fetch the data");
+      }
+    });
 
-  request.addEventListener('readystatechange', () => {
-
-    if(request.readyState === 4 && request.status === 200){
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4){
-      callback('could not fetch the data', undefined);
-    }
-
+    request.open("GET", resource);
+    request.send();
   });
-  
-  request.open('GET', resource);
-  request.send();
+};
+// .then and  . catch
+getTodos("json/luigi.json")
+  .then(data => {
+    console.log("promise resolved:", data);
+  })
+  .catch(err => {
+    console.log("promise rejected:", err);
+  });
 
+// promise example one
+const getSomething = () => {
+  // first return a new Promise  it iwll resolve or be rejected if there is an error
+  // resolve and refject are prarameters ready made in Promis
+  return new Promise((resolve, reject) => {
+    // do something  fetch data
+    // resolve ('some data')
+    reject("some error");
+  });
 };
 
-getTodos('json/luigi.json', (err, data) => {
-  console.log(data);
-  getTodos('json/mario.json', (err, data) => {
-    console.log(data);
-    getTodos('json/shaun.json', (err, data) => {
-      console.log(data);
-    });
+getSomething().then(
+  data => {
+    console.log("promise resolved:", data);
+  },
+  err => {
+    console.log("promise rejected:", err);
+  }
+);
+// using catch  - looks better espcially when chaining promises
+getSomething()
+  .then(data => {
+    console.log("promise resolved:", data);
+  })
+  .catch(err => {
+    console.log("promise rejected:", err);
   });
-});
