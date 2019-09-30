81de347bc902c94248988542d2e26335371a0c0f
AR123456 <coloradoz99@gmail.com>
2019-09-29 19:43:01 -0600

    response status

--- sandbox.js
+++ sandbox.js
@@ -1,16 +1,13 @@
-// XMLHttpRequest is a request object built into the browser
 const request = new XMLHttpRequest();
 
-// propertys and methods are on the XMLHttprequest
-//special event listener "readystatechange"
-request.addEventListener("readystatechange", () => {
-  // console.log(request, request.readyState);
-  if (request.readyState === 4) {
-    // console.log(request);
+request.addEventListener('readystatechange', () => {
+  // console.log(request);
+  if(request.readyState === 4 && request.status === 200){
     console.log(request.responseText);
+  } else if (request.readyState === 4){
+    console.log('could not fetch the data');
   }
 });
-// type of request is frist argument , the second argument is where the request is going to
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-// request.send actualy sends the request
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();
