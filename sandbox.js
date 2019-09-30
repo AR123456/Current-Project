faae6f9c7a3e15005f018644a71216efd61dd5df
AR123456 <coloradoz99@gmail.com>
2019-09-29 19:42:11 -0600

    making http requests

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,16 @@
-console.log(1);
-console.log(2);
-// this is mimicing a HTTP request which will do next lesson
-setTimeout(() => {
-  console.log("callback function fired");
-}, 2000);
+// XMLHttpRequest is a request object built into the browser
+const request = new XMLHttpRequest();
 
-console.log(3);
-console.log(4);
+// propertys and methods are on the XMLHttprequest
+//special event listener "readystatechange"
+request.addEventListener("readystatechange", () => {
+  // console.log(request, request.readyState);
+  if (request.readyState === 4) {
+    // console.log(request);
+    console.log(request.responseText);
+  }
+});
+// type of request is frist argument , the second argument is where the request is going to
+request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+// request.send actualy sends the request
+request.send();
