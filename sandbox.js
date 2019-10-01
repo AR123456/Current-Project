1904b8ee70bdad5cf9fbca557a1e3cc6270422af
AR123456 <coloradoz99@gmail.com>
2019-10-01 06:58:32 -0600

    fetch api

--- sandbox.js
+++ sandbox.js
@@ -1,37 +1,31 @@
-const getTodos = resource => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener("readystatechange", () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        resolve(data);
-      } else if (request.readyState === 4) {
-        reject("could not fetch the data");
-      }
-    });
-
-    request.open("GET", resource);
-    request.send();
-  });
-};
-
-getTodos("json/luigi.json")
-  .then(data => {
-    console.log("promise 1 resolved:", data);
-    //returning a promise
-    return getTodos("json/mario.json");
+// fetch API
+// this is native and built into javascript  can replace the old htp request
+// a function in the language
+// fetch has one arument which is the resource that needs to be fetched - fetch returns a promis so can use .then
+fetch("json/luigi.json")
+  // .then is for the success message
+  .then(response => {
+    //console.log(response);
+    // response.json gets data and parses it to make it useful in the code.
+    return response.json();
   })
+  // becasue we have a promise returned we can use .then and chain them too
   .then(data => {
-    // do this after 1st reques is done
-    console.log("promise 2 resolved:", data);
-    // return here gets the next promise
-    return getTodos("json/shaun.json");
+    console.log(data);
   })
-  .then(data => {
-    console.log("promise 3 resolved:", data);
-  })
+  // .catch fires a call back when /if there is an error
+  //  with the fetch API is is only ever rejected when there is a network error, if ther is just an error with like a typo in the end point it still resolves.  But, in the response thee is a status with a number for the type of error and statusText of the error
+
   .catch(err => {
-    //catch would apply to any of the requests
-    console.log("promise rejected:", err);
+    console.log(err);
   });
+
+//////////////////////////////
+// fetch("json/luigis.json")
+//   .then(response => {
+//     console.log("resolved", response);
+//   })
+//   .catch(err => {
+//     console.log("rejected", err);
+//   });
+// Three steps   1) fetch data  2) take response  3) return response
