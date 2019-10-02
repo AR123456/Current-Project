614673662e2f9f595b3d25c2f98774614e93af95
AR123456 <coloradoz99@gmail.com>
2019-10-02 05:37:19 -0600

    async and await

--- sandbox.js
+++ sandbox.js
@@ -1,31 +1,46 @@
-// fetch API
-// this is native and built into javascript  can replace the old htp request
-// a function in the language
-// fetch has one arument which is the resource that needs to be fetched - fetch returns a promis so can use .then
+// async & await
+//  asynchronous code can be set off into a single function
+// then use await  inside to chain promesies together
+
+// all the async code (code that is going out somewhere to get something goes here)
+// just add async between the = and () to make the function async, this key word will cause the function
+// to return a promise regardles of what is inside the curly braces
+const getTodos = async () => {
+  // use await instead of .then  it stalls JS until promise come back.
+  // but an async function is not blocking > async is not blocking the rest of the
+  let response = await fetch("json/luigi.json");
+  let data = await response.json();
+  return data;
+};
+
+console.log(1);
+console.log(2);
+
+getTodos().then(data => console.log("resolved:", data));
+
+console.log(3);
+console.log(4);
+
+console.log(getTodos());
+////////////////////////////////////////////////////////////////////////////////
+// this is the code if not using await and response
 fetch("json/luigi.json")
-  // .then is for the success message
   .then(response => {
-    //console.log(response);
-    // response.json gets data and parses it to make it useful in the code.
+    console.log("resolved", response);
     return response.json();
   })
-  // becasue we have a promise returned we can use .then and chain them too
   .then(data => {
     console.log(data);
   })
-  // .catch fires a call back when /if there is an error
-  //  with the fetch API is is only ever rejected when there is a network error, if ther is just an error with like a typo in the end point it still resolves.  But, in the response thee is a status with a number for the type of error and statusText of the error
-
   .catch(err => {
     console.log(err);
   });
 
-//////////////////////////////
-// fetch("json/luigis.json")
-//   .then(response => {
-//     console.log("resolved", response);
-//   })
-//   .catch(err => {
-//     console.log("rejected", err);
-//   });
-// Three steps   1) fetch data  2) take response  3) return response
+////////////////////////////////////////
+//this is the code if using await and response
+const getTodos = async () => {
+  const response = await fetch("json/luigi.json");
+  const data = await response.json();
+  console.log(data);
+};
+getTodos();
