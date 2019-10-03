a7408d1250e3b2527c98513e6fb37925dcf72348
AR123456 <coloradoz99@gmail.com>
2019-10-03 06:16:46 -0600

    updates to ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,37 @@
 const cityForm = document.querySelector("form");
+// for the card class
+const card = document.querySelector(".card");
+// for the detail class
+const details = document.querySelector(".details");
+// this is updating the dom with the "data" we get back from the updateCity function
+const updateUI = data => {
+  // doing this so that we can just reference data.cityDets as cityDets and data.weather as weather
+  const cityDets = data.cityDets;
+  const weather = data.weather;
+
+  // update details template
+  // using a template string to send data to the dom
+  //Sean just did a copy paste from the HTML file as a starter and then used ${} to switch the text out for the data that came back
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Imperial.Value}</span>
+      <span>&deg;F</span>
+    </div>
+  `;
+
+  // remove the d-none class if present - addded to the HTML
+  //
+  if (card.classList.contains("d-none")) {
+    //   if present remove it
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async city => {
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
-  // becasue the property and value are the same name can use object shorthand notation to just use one
   return { cityDets, weather };
 };
 
@@ -17,6 +45,7 @@ cityForm.addEventListener("submit", e => {
 
   // update the ui with new city
   updateCity(city)
-    .then(data => console.log(data))
+    // pass the data we get back into the updateUi function
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
