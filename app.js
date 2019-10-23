4527e7abe510d3e81f64c86e0f48e86a79a468c4
AR123456 <coloradoz99@gmail.com>
2019-10-23 06:32:02 -0600

    Nodemailer Contact form

--- app.js
+++ app.js
@@ -17,4 +17,54 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.render("contact");
 });
+// post route
+app.post("/send", (req, res) => {
+  const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>
+  <li>Name: ${req.body.name}</li>
+  <li>Company: ${req.body.company}</li>
+  <li>Email: ${req.body.email}</li>
+  <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+  <p>${req.body.message}</p>
+  `;
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: " ",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: " ", // mailtrap io
+      pass: " " // mailtrap io
+    },
+    // just for testing on local host
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Test" 12343', // sender address
+    to: "test@gmail.com", // list of receivers
+    subject: "Testing Node Contact Request", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body- the output variable
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render("contact", { msg: "Email has been sent" });
+  });
+});
+
 app.listen(3000, () => console.log("Server started..."));
