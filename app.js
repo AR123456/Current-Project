39643d0aa25a5b4f624c42a32820afbcb2db4d57
AR123456 <coloradoz99@gmail.com>
2019-10-24 06:24:45 -0600

    implimenting sendgrid

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const path = require("path");
 const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const app = express();
 // view engine handlebars
@@ -31,37 +32,35 @@ app.post("/send", (req, res) => {
     <h3>Message</h3>
   <p>${req.body.message}</p>
   `;
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: " ",
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: " ", // mailtrap io
-      pass: " " // mailtrap io
-    },
-    // just for testing on local host
-    tls: {
-      rejectUnauthorized: false
-    }
-  });
+
+  /// for the sendgrid implementation
+  const transporter = nodemailer.createTransport(
+    sendgridTransport({
+      auth: {
+        api_key: " "
+      }
+    })
+  );
 
   // setup email data with unicode symbols
   let mailOptions = {
-    from: '"Test" 12343', // sender address
-    to: "test@gmail.com", // list of receivers
+    to: req.body.email, // list of receivers
+    from: '"mail@SpinRecipe.com" test', // sender address
     subject: "Testing Node Contact Request", // Subject line
-    text: "Hello world?", // plain text body
+    // text: "Hello world?", // plain text body
     html: output // html body- the output variable
   };
+
+  // sendgrid implimentation
 
   // send mail with defined transport object
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       return console.log(error);
     }
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // console.log("Message sent: %s", info.messageId);
+    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    console.log(transporter.sendMail);
 
     res.render("contact", { msg: "Email has been sent" });
   });
